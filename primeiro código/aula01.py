afb8effb8471034f7665db01f028e89d210a4c08
rannyzita <142625865+rannyzita@users.noreply.github.com>
2024-02-08 07:19:49 -0300

    Add files via upload

---
+++ primeiro código/aula01.py
@@ -0,0 +1,26 @@
+#aula 01 
+
+print(7+1)
+
+print('7'+'1')
+
+print('Olá', 5)
+
+print('\n')
+
+#Registrar nome, idade, e peso de uma pessoa
+
+nome = 'guanabara'
+idade = 25
+altura = 75.8
+
+print(nome, idade, altura)
+
+print('\n')
+
+#input == leia
+nome = input('Qual o seu nome?')
+idade = input ('Qual a sua idade?')
+altura = input('Qual a sua altura?')
+
+print(nome, idade, altura)
