24d567548ea988323d94f566a2276632a1a72ecb
rannyzita <142625865+rannyzita@users.noreply.github.com>
2024-03-24 00:17:51 -0300

    Update fibonacci.py

--- exercicios/Exercicios de Alex/Lista/Recursividade/fibonacci.py
+++ exercicios/Exercicios de Alex/Lista/Recursividade/fibonacci.py
@@ -1,36 +1,12 @@
-def fibonacci(max_vezes, resultado = None):
-    
-    #se resultado estiver vazia, ou seja, sempre verdadeira...
-    if resultado is None:
-        resultado = [0, 1]
+def fibonacci(n):
+      if n <= 1:
+            return 1
+      else:
+            return fibonacci(n-1) + fibonacci(n-2)
+            
+n = int(input('Digite o enésimo numero: '))
 
-    #verifica se tem menos de 2 elementos na lista
+resultado = fibonacci(n)
 
-    if len(resultado) < 2:
-
-        n1 = 0
-        n2 = 1
-
-    #se o numero de elementos da lista "resultado" for menor que
-    #n de vezes
-    #do usuario...
-
-    if len(resultado) < max_vezes:
-        soma = resultado[-1] + resultado[-2]
-        
-        resultado.append(soma)
-        return fibonacci(max_vezes, resultado)
-        
-    #retorna todos os numeros de fibonacci
-    return resultado
-
-while True:
-
-    max_vezes = int(input('\nQuantos números de Fibonacci você quer gerar? \n'))
-
-    resultado = fibonacci(max_vezes)
-
-    #imprimi todos os numeros de fibonacci
-    print(resultado)
-    
-    break
+for count in range(n):
+    print(fibonacci(count), end=" ")
